4c4acb0c875fd67b24916b9250fa4b72443b378a
OzcanI <ibosnfs1997@gmail.com>
2021-07-15 01:51:20 +0300

    first

--- _routes/index.js
+++ _routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res)=>{
+    return res.send('<a href="https://github.com/OzcanI/Assignment-Project">https://github.com/OzcanI/Assignment-Project</a>')
+})
 router.use('/users', require('./user_router'))
 
 module.exports = router;
